9ee6c31a2c51ad1b56697cf9d738de860b6b2b88
Damianswankkd4 <52354566+Damianswankkd4@users.noreply.github.com>
2020-03-20 10:56:11 -0400

    Added Meme Command

--- index.js
+++ index.js
@@ -161,6 +161,9 @@ bot.on('message', msg => {
 
                         msg.author.send(Embed);
                         break;
+                        case 'Meme':
+                            image2(msg);
+                            break;
 
 
 
@@ -174,6 +177,41 @@ function image(msg) {
 
     var options = {
         url: "http://results.dogpile.com/serp?qc=images&q=" + "Cute Cat Images",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+
+    request(options, function (error, response, responseBody) {
+        if (error) {
+            return;
+        }
+
+        $ = cheerio.load(responseBody);
+
+
+        var links = $(".image a.link");
+
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+
+        console.log(urls);
+        if (!urls.length) {
+
+            return;
+
+        }
+
+        //send request
+        msg.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+
+    });
+}
+function image2(msg) {
+
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "Clean Memes",
         method: "GET",
         headers: {
             "Accept": "text/html",
